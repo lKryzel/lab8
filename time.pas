e808d837104145ecf8905205a20e30ce0ed6235a
Kachalov Nikita <kachalov1015@gmail.com>
2024-10-05 06:20:03 +0300

    second version

--- time.pas
+++ time.pas
@@ -1,5 +1,6 @@
 ﻿begin
   var year := ReadInteger('Years');
-  if year mod 4 = 0 and (year mod 100 <> 0 or year mod 400 = 0) then
-    Println('Високосный'); 
+  if (year mod 4 = 0) and ((year mod 100 <> 0) or (year mod 400 = 0)) then
+    Println('Високосный', 366)
+  else Println('Невисокосный', 365);
 end.
