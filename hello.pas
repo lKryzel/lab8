ad97ad55701411e0507ecf06fb16921b82a0374b
Kachalov Nikita <kachalov1015@gmail.com>
2024-10-05 06:11:49 +0300

    New version

--- hello.pas
+++ hello.pas
@@ -1,3 +1,11 @@
 ﻿begin
-  
+  var time := ReadInteger('Который час?');
+  if time in 4..10 then
+    println('Доброе утро, мир!');
+  if time in 11..16 then
+    println('Добрый день. мир!');
+  if time in 17..22 then
+    println('Добрый вечер, мир!');
+  if (time in 0..3) or (time = 23) then
+    println('Доброй ночи,мир!');
 end.
